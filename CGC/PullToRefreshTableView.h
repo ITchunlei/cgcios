837248d1245babf3d5fbb8fb8ba96ecd2d10f519
chunlei <ITchunlei@163.com>
2014-09-01 22:08:29 +0800

    all

---
+++ CGC/PullToRefreshTableView.h
@@ -0,0 +1,13 @@
+//
+//  PullToRefreshTableView.h
+//  CGC
+//
+//  Created by chunlei on 14-9-1.
+//  Copyright (c) 2014年 chunlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PullToRefreshTableView : UITableView
+
+@end
---
+++ CGC/PullToRefreshTableView.m
@@ -0,0 +1,31 @@
+//
+//  PullToRefreshTableView.m
+//  CGC
+//
+//  Created by chunlei on 14-9-1.
+//  Copyright (c) 2014年 chunlei. All rights reserved.
+//
+
+#import "PullToRefreshTableView.h"
+
+@implementation PullToRefreshTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
