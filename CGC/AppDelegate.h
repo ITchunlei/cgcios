d777ecb915b1b0cda4d92993e18db87340979d9f
chunlei <ITchunlei@163.com>
2014-09-01 20:54:37 +0800

    Initial Commit

---
+++ CGC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CGC
+//
+//  Created by chunlei on 14-9-1.
+//  Copyright (c) 2014年 chunlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
